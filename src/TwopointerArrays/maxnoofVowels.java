029a5f2745de701cf490be1ddaee76244ffdfeb5
devas <devas@LAPTOP-8JVS74OV>
2022-10-23 15:10:11 +0530

    added index incrementor

--- src/TwopointerArrays/maxnoofVowels.java
+++ src/TwopointerArrays/maxnoofVowels.java
@@ -1,5 +1,7 @@
 package TwopointerArrays;
 
+import org.junit.Test;
+
 public class maxnoofVowels 
 {
 	/*Given a string s and a number k, find the maximum number of vowels in substring of size k.
@@ -8,42 +10,44 @@ public class maxnoofVowels
 	/*
 	 * work -1 orka-2 rkaa -2 kaat-2 aatt-2 atte-2 ttec-1 tech-1
 	 */
-public void example()
-{
-String s="workaattech";
-int k=3;
-}
-	
-public static boolean[] vowels =new boolean[256];
-static 
-{
-	vowels['a'] =true;
-	vowels['e'] =true;
-	vowels['i'] =true;
-	vowels['o'] =true;
-	vowels['u'] =true;
-	
-}
-public int findmaxVowels(String s ,int k)
-{
-	
-int index=0;
-int count=0, max=0;
+	@Test
+	public void example()
+	{
+		String s="workaattech";
+		int k=3;
+		System.out.println(findmaxVowels(s,k));
+	}
 
-while(index<k)
-{
-if(vowels[s.charAt(index++)])count++;
+	public static boolean[] vowels =new boolean[256];
+	static 
+	{
+		vowels['a'] =true;
+		vowels['e'] =true;
+		vowels['i'] =true;
+		vowels['o'] =true;
+		vowels['u'] =true;
 
-}
-max=count;
-while(index<s.length())
-{
-if(!vowels[s.charAt(index)])count--;
-if(vowels[s.charAt(index)])count++;
-max=Math.max(count, max);
-}	
-return max;
-	
+	}
+	public int findmaxVowels(String s ,int k)
+	{
 
-}
+		int index=0;
+		int count=0, max=0;
+
+		while(index<k)
+		{
+			if(vowels[s.charAt(index++)])count++;
+
+		}
+		max=count;
+		while(index<s.length())
+		{
+			if(!vowels[s.charAt(index++)])count--;
+			if(vowels[s.charAt(index++)])count++;
+			max=Math.max(count, max);
+		}	
+		return max;
+
+
+	}
 }
