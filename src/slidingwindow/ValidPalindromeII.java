599bb0b160746af392ef9325b359310d4947c189
devas <devas@LAPTOP-8JVS74OV>
2022-12-04 01:08:13 +0530

    ValidPalindrome II 680

--- src/slidingwindow/ValidPalindromeII.java
+++ src/slidingwindow/ValidPalindromeII.java
@@ -1,9 +1,40 @@
 package slidingwindow;
 
+import java.util.HashMap;
+import java.util.Set;
+
 import org.junit.Test;
 
 public class ValidPalindromeII 
 {
+	/*
+	 * 680. Valid Palindrome II
+	 * 
+	 * Given a string s, return true if the s can be palindrome after deleting at
+	 * most one character from it.
+	 * 
+	 * 
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: s = "aba" Output: true 
+	 * 
+	 * Example 2:
+	 * 
+	 * Input: s = "abca" Output: true Explanation: You could delete the character
+	 * 'c'. 
+	 * 
+	 * Example 3:
+	 * 
+	 * Input: s = "abc" 
+	 * Output: false 
+	 * 
+	 * Constraints:
+	 * 
+	 * 1 <= s.length <= 105 s consists of lowercase English letters.
+	 */
+	
+	
 	@Test
 	public void example1()
 	{
@@ -149,4 +180,8 @@ public class ValidPalindromeII
 		}
 		return true;
 	}
-}
+	
+	
+		
+	}
+
