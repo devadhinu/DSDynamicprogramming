595e2129b69da5f00a8f074da4584e71e755c9c2
devas <devas@LAPTOP-8JVS74OV>
2022-12-06 02:37:43 +0530

    move zeros optimized

--- src/TwopointerArrays/Movezerostwopinter.java
+++ src/TwopointerArrays/Movezerostwopinter.java
@@ -2,6 +2,7 @@ package TwopointerArrays;
 
 import java.util.Arrays;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /*
@@ -17,22 +18,35 @@ Note that you must do this in-place without making a copy of the array.
 public class Movezerostwopinter {
 
 	@Test
-	public void example1()
-	{
-		int[] nums= {0,1,0,3,12};
-		movezerostoLast(nums);
+	public void positive() {
+		//Positive Test Data
+		int[] nums = {0,1,0,3,12};
+		int[] output = {1,3,12,0,0};
+		Assert.assertArrayEquals(moveZeroes(nums), output);
 	}
-
+	
 	@Test
-	public void example2()
-	{
-		int[] nums= {0};	
+	public void edge() {
+		//Edge Case Test Data
+		int[] nums = {0};
+		int[] output = {0};
+		Assert.assertArrayEquals(moveZeroes(nums), output);
 	}
-
+	
 	@Test
-	public void example3()
-	{
-
+	public void negative() {
+		//Negative Test Data
+		int[] nums = {0,0,0,3,0,0};
+		int[] output = {3,0,0,0,0,0};
+		Assert.assertArrayEquals(moveZeroes(nums), output);
+	}
+	
+	@Test
+	public void edge2() {
+		//Negative Test Data
+		int[] nums = {1,0,1};
+		int[] output = {1,1,0};
+		Assert.assertArrayEquals(moveZeroes(nums), output);
 	}
 
 	private int[] movezerostoLast(int[] nums) 
@@ -60,18 +74,18 @@ public class Movezerostwopinter {
 
 	}
 	//optimized
-	/*
-	 * private int[] movezerostoLast(int[] nums) { int left=0,right=left+1; int
-	 * temp;
-	 * 
-	 * while(right<nums.length) { if(nums[left]==0 && nums[right]!=0) {
-	 * temp=nums[left]; nums[left++]=nums[right]; nums[right]=temp; } right++;
-	 * if(nums[left]==0 && nums[right]==0) { right++; } else if(nums[left]!=0 &&
-	 * nums[right]!=0) left++;right++;
-	 * 
-	 * } System.out.println(Arrays.toString(nums)); return nums;
-	 * 
-	 * }
-	 */
-
+	 public int[] moveZeroes(int[] nums)
+	 {
+		 int index=0;
+		 for(int i=0;i<nums.length;i++)
+		 {
+			 if(nums[i]!=0)nums[index++]=nums[i];
+		 }
+		 while(index<nums.length)
+		 {
+			 nums[index++]=0;
+		 }
+		 return nums;
+	 }
+	
 }
